4039c7d4f3d7b79e29e0e0af48e178ec11499e74
ca1yz <cai.yz@outlook.com>
2025-06-20 12:25:56 +0800

    replace requests with urllib

--- src/_config_endpoint.py
+++ src/_config_endpoint.py
@@ -1,4 +1,4 @@
-import requests
+import urllib.request
 import os
 import logging
 
@@ -20,7 +20,9 @@ def config_endpoint():
     # Use a specific check for the Hugging Face source
     if os.environ['MINERU_MODEL_SOURCE'] == 'huggingface':
         try:
-            response = requests.head(model_list_url, timeout=TIMEOUT)
+            # response = requests.head(model_list_url, timeout=TIMEOUT)
+            request = urllib.request.Request(model_list_url, method='HEAD')
+            response = urllib.request.urlopen(request, timeout=TIMEOUT)
             
             # Check for any successful status code (2xx)
             if response.ok:
@@ -29,7 +31,7 @@ def config_endpoint():
             else:
                 logging.warning(f"Hugging Face endpoint returned a non-200 status code: {response.status_code}")
 
-        except requests.exceptions.RequestException as e:
+        except Exception as e:
             logging.info(f"Failed to connect to Hugging Face at {model_list_url}: {e}")
 
         # If any of the above checks fail, switch to modelscope
@@ -38,11 +40,13 @@ def config_endpoint():
     
     elif os.environ['MINERU_MODEL_SOURCE'] == 'modelscope':
         try:
-            response = requests.head(model_list_url, timeout=TIMEOUT)
+            request = urllib.request.Request(model_list_url, method='HEAD')
+            response = urllib.request.urlopen(request, timeout=TIMEOUT)
+
             if response.ok:
                 logging.info(f"Successfully connected to ModelScope. Using 'modelscope' as model source.")
                 return True
-        except requests.exceptions.RequestException as e:
+        except Exception as e:
             logging.info(f"Failed to connect to ModelScope at {model_list_url}: {e}")
         
     elif os.environ['MINERU_MODEL_SOURCE'] == 'local':
